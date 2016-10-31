d2586b5928c68f236a4e4ad9e0145dc02e5e5435
echemoo <echemoo@gmail.com>
2016-10-31 21:52:55 +0000

    add approximately equal of

--- src/approx.js
+++ src/approx.js
@@ -48,3 +48,37 @@ exports.equal = function equal(a, b, epsilon) {
   }
 };
 
+/**
+ * Test whether all values in two objects or arrays are approximately equal.
+ * Will deep compare all values of Arrays and Objects element wise.
+ * @param {*} a
+ * @param {*} b
+ */
+exports.deepEqual = function deepEqual(a, b) {
+  var prop, i, len;
+
+  if (Array.isArray(a) && Array.isArray(b)) {
+    assert.equal(a.length, b.length, a + ' ~= ' + b);
+    for (i = 0, len = a.length; i < len; i++) {
+      deepEqual(a[i], b[i]);
+    }
+  }
+  else if (a instanceof Object && b instanceof Object) {
+    for (prop in a) {
+      if (a.hasOwnProperty(prop)) {
+        assert.ok(b.hasOwnProperty(prop), a[prop] + ' ~= ' + b[prop]);
+        deepEqual(a[prop], b[prop]);
+      }
+    }
+
+    for (prop in b) {
+      if (b.hasOwnProperty(prop)) {
+        assert.ok(a.hasOwnProperty(prop), a[prop] + ' ~= ' + b[prop]);
+        deepEqual(a[prop], b[prop]);
+      }
+    }
+  }
+  else {
+    exports.equal(a, b);
+  }
+};
